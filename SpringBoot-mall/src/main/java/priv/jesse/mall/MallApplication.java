ac40d8412b7b9bbb9c28db1ed3ef8b0c1ecccf32
WangAnXiang-wq <2514820726@qq.com>
2020-04-07 17:46:08 +0800

    一 Signed-off-by: WangAnXiang-wq <2514820726@qq.com>

---
+++ SpringBoot-mall/src/main/java/priv/jesse/mall/MallApplication.java
@@ -0,0 +1,20 @@
+package priv.jesse.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 程序启动入口
+ *
+ *
+ *
+ */
+
+@SpringBootApplication
+public class MallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallApplication.class, args);
+	}
+}
